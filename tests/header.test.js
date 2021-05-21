8ff14c6ff17f682d16bf407b4977b29f5a20d708
khoi.tran@email.com <dinhkhoi.tran@serdao.fr>
2021-05-21 21:13:07 +0700

    test case for no log in

--- tests/header.test.js
+++ tests/header.test.js
@@ -21,8 +21,41 @@ test('clicking login starts oauth flow', async()=>{
     console.log(url)
 })
 
-test.only('When signed in, shows logout button', async()=>{
+test('When signed in, shows logout button', async()=>{
     await customPage.login()
     const text = await customPage.getContentsOf('a[href="/auth/logout"]')
     expect(text).toEqual('Logout')
+})
+
+describe('User is not logged in', async () => {
+    test('User cannot create blog post', async() => {
+        const result = await customPage.evaluate(
+            () => {
+                return fetch('/api/blogs', {
+                    method: 'POST',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ title: 'My title', content: 'My content'})
+                }).then(res => res.json())
+            }
+        )
+        expect(result).toEqual({error: 'You must log in!'})
+    })
+
+    test.only('User cannot get a list of posts', async() => {
+        const result = await customPage.evaluate(
+            () => {
+                return fetch('/api/blogs', {
+                    method: 'GET',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                }).then(res => res.json())
+            }
+        )
+        expect(result).toEqual({error: 'You must log in!'})
+    })
 })
