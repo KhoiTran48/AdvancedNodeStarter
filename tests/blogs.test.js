6e932763622fa5bb9b3953b0eb39c165babda951
user <user@users-MacBook-Pro.local>
2021-05-22 16:55:42 +0700

    add UT for blogs

---
+++ tests/blogs.test.js
@@ -0,0 +1,102 @@
+const CustomPage = require('./helpers/customPage')
+
+let customPage
+beforeEach(async() => {
+    customPage = await CustomPage.build()
+    await customPage.goto('localhost:3000')
+})
+
+afterEach(async()=>{
+    await customPage.close()
+})
+
+describe('When logged in', async() => {
+    beforeEach(async() => {
+        await customPage.login()
+        await customPage.click('a[href="/blogs/new"]')
+    })
+
+    test('When logged in, can see log create form', async()=>{
+        const blogTitleLabel = await customPage.getContentsOf('.title label')
+        expect(blogTitleLabel).toEqual('Blog Title')
+    })
+
+    describe('And using invalid inputs', async() => {
+        beforeEach(async() => {
+            await customPage.click('form button')
+        })
+
+        test('the form shows error message', async() => {
+            const titleError = await customPage.getContentsOf('.title .red-text')
+            const contentError = await customPage.getContentsOf('.content .red-text')
+
+            expect(titleError).toEqual('You must provide a value')
+            expect(contentError).toEqual('You must provide a value')
+        })
+
+    })
+
+    describe('And using valid inputs', async() => {
+        beforeEach(async() => {
+            await customPage.type('.title input', 'My title')
+            await customPage.type('.content input', 'My content')
+
+            // click submit button
+            await customPage.click('form button')
+        })
+        test('show review page after submit', async() => {
+            const headerOfReviewPage = await customPage.getContentsOf('form h5')
+            expect(headerOfReviewPage).toEqual('Please confirm your entries')
+        })
+        test('redirect to list blogs page after confirmation', async() => {
+
+            // click save button
+            await customPage.click('button.green')
+
+            // wait for page /blogs appear
+            await customPage.waitFor('.card')
+
+            const title = await customPage.getContentsOf('.card-title')
+            const content = await customPage.getContentsOf('p')
+
+            expect(title).toEqual('My title')
+            expect(content).toEqual('My content')
+        })
+    })
+})
+
+describe('User is not logged in', async () => {
+
+    test('User cannot get a list of posts', async() => {
+        const result = await customPage.evaluate(
+            () => {
+                return fetch('/api/blogs', {
+                    method: 'GET',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                }).then(res => res.json())
+            }
+        )
+        expect(result).toEqual({error: 'You must log in!'})
+    })
+
+    test('User cannot create blog post', async() => {
+        const result = await customPage.evaluate(
+            () => {
+                return fetch('/api/blogs', {
+                    method: 'POST',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ title: 'My title', content: 'My content'})
+                }).then(res => res.json())
+            }
+        )
+        expect(result).toEqual({error: 'You must log in!'})
+    })
+
+    
+})
--- tests/helpers/customPage.js
+++ tests/helpers/customPage.js
@@ -27,18 +27,15 @@ class CustomPage {
 
         await this.page.setCookie({ name: 'session', value: session})
         await this.page.setCookie({ name: 'session.sig', value: sig})
-        await this.page.goto('localhost:3000')
-
-        // chromium take so long to generate the page
-        // and the test is not see the element 'a[href="/auth/logout"]'
-        // so the we have to wait until the logout button element is appear on the page
-        // using waitFor
-        // if waiting more than 5000ms, we will take timeout
-        // -> let's try :))
-        await this.page.waitFor('a[href="/auth/logout"]')
+        await this.page.goto('localhost:3000/blogs')
     }
 
     async getContentsOf(selector) {
+        // chromium take so long to generate the page
+        // so the test have to wait until the selected element is appear on the page
+        // using waitFor
+        // if waiting more than 5000ms, we will take timeout
+        await this.page.waitFor(selector)
         return await this.page.$eval(selector, el => el.innerHTML)
     }
 }
